f4619b95f3bb86737a14f0acaf7fd932557bf965
Karen Etheridge <ether@cpan.org>
2015-09-23 19:37:38 -0700

    increment $VERSION after 0.023 release

--- lib/Acme/CPANAuthors/Nonhuman.pm
+++ lib/Acme/CPANAuthors/Nonhuman.pm
@@ -5,7 +5,7 @@ package Acme::CPANAuthors::Nonhuman;
 # KEYWORDS: acmeism cpan authors animals fun
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.023';
+our $VERSION = '0.024';
 
 use utf8;
 
